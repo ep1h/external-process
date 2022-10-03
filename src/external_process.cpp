41c89cc381157f0b7271cf43acf47dd706917f8a
ep1h <ep1hxx@gmail.com>
2022-10-03 08:10:53 -0700

    Create ExternalProcess class
    The class will contain the main functionality for interacting with
    external processes.

---
+++ src/external_process.cpp
@@ -0,0 +1,20 @@
+/**-----------------------------------------------------------------------------
+; @file external_process.cpp
+;
+; @brief
+;   The file contains class members and methods specification of ExternalProcess
+;   class.
+;
+; @author ep1h
+;-----------------------------------------------------------------------------*/
+#include "external_process.hpp"
+
+using namespace P1ExternalProcess;
+
+ExternalProcess::ExternalProcess(void)
+{
+}
+
+ExternalProcess::~ExternalProcess(void)
+{
+}
---
+++ src/external_process.hpp
@@ -0,0 +1,23 @@
+/**-----------------------------------------------------------------------------
+; @file external_process.hpp
+;
+; @author ep1h
+;-----------------------------------------------------------------------------*/
+#ifndef EXTERNAL_PROCESS_HPP
+#define EXTERNAL_PROCESS_HPP
+
+namespace P1ExternalProcess
+{
+
+class ExternalProcess
+{
+public:
+    ExternalProcess(void);
+    ~ExternalProcess(void);
+
+private:
+};
+
+} /* namespace P1ExternalProcess */
+
+#endif /* EXTERNAL_PROCESS_HPP */

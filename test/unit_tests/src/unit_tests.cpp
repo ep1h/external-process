cf7c63c8800765a9d12f298ed748d9607a7e21fe
ep1h <ep1hxx@gmail.com>
2022-10-05 21:57:44 +0300

    Implement tests for 'get_process_id_by_process_name' method

---
+++ test/unit_tests/src/unit_tests.cpp
@@ -0,0 +1,36 @@
+/**-----------------------------------------------------------------------------
+; @file unit_tests.cpp
+;
+; @brief
+;   The file contains tests for ExternalProcess class.
+;
+; @author ep1h
+;-----------------------------------------------------------------------------*/
+#include "test.h"
+
+#include "../../../src/external_process.hpp"
+
+/* Remote process name. Testing functionality of ExternalProcess class will be
+ * performed based on interaction with this process. */
+// TODO: For greater reliability of the tests, this should not be the current
+//       process.
+static const char test_application[] = "test.exe";
+
+using P1ExternalProcess::ExternalProcess;
+
+TEST_BEGIN(get_process_id_by_non_existent_process_name)
+{
+    ExternalProcess ep((uint32_t)0);
+    EXPECT(ep.get_process_id_by_process_name("!@#$^&*()_+"), 0);
+}
+TEST_END
+
+TEST_BEGIN(get_process_id_by_existent_process_name)
+{
+    ExternalProcess ep((uint32_t)0);
+    EXPECT_NOT_ZERO(ep.get_process_id_by_process_name(test_application));
+}
+TEST_END
+
+RUN_TESTS(get_process_id_by_non_existent_process_name,
+          get_process_id_by_existent_process_name);
